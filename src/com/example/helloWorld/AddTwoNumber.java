dbef9b5383907d2591626a7c255e514133cfef03
Fifi Makeunchea <fmakeunch@gmail.com>
2020-07-08 12:26:29 -0400

    Add 2 two number

---
+++ src/com/example/helloWorld/AddTwoNumber.java
@@ -0,0 +1,9 @@
+package com.example.helloWorld;
+
+public class AddTwoNumber {
+    public static void main(String[] args) {
+        int num1= 5, num2= 7;
+        int total =num1 + num2;
+        System.out.print("The sum of num1 and num2 is: " + total);
+    }
+}
