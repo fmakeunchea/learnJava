9dea6a58197408fc5d467342ad93b5f0cb6c3160
Fifi Makeunchea <fmakeunch@gmail.com>
2020-07-13 17:06:27 -0400

    Library management System Project

--- src/com/example/helloWorld/AddTwoNumber.java
+++
@@ -1,9 +0,0 @@
-package com.example.helloWorld;
-
-public class AddTwoNumber {
-    public static void main(String[] args) {
-        int num1= 5, num2= 7;
-        int total =num1 + num2;
-        System.out.print("The sum of num1 and num2 is: " + total);
-    }
-}
